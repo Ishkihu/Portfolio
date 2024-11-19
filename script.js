d61ac9a90931ff5cb9cfd5e480d7db0a03d9b5cb
JV-Esquejo <esquejojohnvictor@gmail.com>
2024-11-19 22:36:49 +0800

    Added more functionality

--- script.js
+++ script.js
@@ -28,4 +28,28 @@ document.addEventListener("DOMContentLoaded", function() {
             hamburger.classList.remove('active');
         }
     });
+
+    const items = document.querySelectorAll('.timeline-item');
+
+    const isInViewport = (el) => {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+
+    const run = () => {
+        items.forEach(item => {
+            if (isInViewport(item)) {
+                item.classList.add('visible');
+            }
+        });
+    };
+
+    window.addEventListener('load', run);
+    window.addEventListener('resize', run);
+    window.addEventListener('scroll', run);
 });
