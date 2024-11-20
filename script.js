9d6f2d11f6da3fc702e48ed0722ea3f5ee9fe96b
JV-Esquejo <esquejojohnvictor@gmail.com>
2024-11-20 11:21:26 +0800

    Added Contact Section Design and Script

--- script.js
+++ script.js
@@ -52,4 +52,22 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('load', run);
     window.addEventListener('resize', run);
     window.addEventListener('scroll', run);
+
+    const form = document.getElementById('contact-form');
+    const successMessage = document.getElementById('success-message');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        // Show the success message
+        successMessage.style.display = 'block';
+
+        // Hide the success message after the animation ends
+        setTimeout(function() {
+            successMessage.style.display = 'none';
+        }, 3000);
+
+        // Optionally, you can reset the form fields
+        form.reset();
+    });
 });
